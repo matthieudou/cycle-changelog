c2f763bc21933a678b3df2e3376a94aca9786e52
Matthieu d'Oultremont <matthieu.doultremont@gmail.com>
2024-09-12 15:53:14 +0200

    chore: improve codebase & add packages

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,23 @@
 import type { Config } from "tailwindcss";
+import pluginTypography from "@tailwindcss/typography";
 
 const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./features/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      colors: {
+        "blue-cycle": "#2B44E7",
+      },
       fontFamily: {
         sans: ["Inter", "sans-serif"],
-      }
+      },
     },
   },
-  plugins: [],
+  plugins: [pluginTypography],
 };
 export default config;
